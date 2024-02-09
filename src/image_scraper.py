714af6efda3ef253f8dc9758e8888352f17a83f3
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-09 15:24:14 +0530

    Create image_scraper.py
    Keeping everything simple. Will rename everything later. This will be a replacement for text_parse_utils,py

---
+++ src/image_scraper.py
@@ -0,0 +1,116 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import os
+import itertools
+import time
+
+
+def parse_data(str_target, ref_text):
+    files = []
+    while str_target in ref_text:
+        count = 1
+        st_index = ref_text.find(str_target) + 7
+        for r in range(st_index, len(ref_text) + 1):
+            if ref_text[r:r + 2] == '[[':
+                count += 1
+            if ref_text[r:r + 2] == ']]':
+                count -= 1
+                if count == 0:
+                    text = ref_text[st_index:r]
+                    files.append(text)
+                    ref_text = ref_text[r + 2:]
+                    break
+    return files
+
+
+def process_img_tag(text):
+    parsing_errors = None
+    text = text.replace('[[', '').replace(']]', '')
+    text_space = text.split('|')
+    name = text_space[0]
+    link = 'https://commons.wikimedia.org/wiki/File:' + '_'.join(name.split()) + "#file"
+    if len(text_space) > 3:
+        text_dict = {name: {'image_url': link,
+                            'type': text_space[1],
+                            'align': text_space[2],
+                            'Caption': text_space[3]
+                            }
+                     }
+    elif len(text_space) == 3:
+        text_dict = {name: {'image_url': link,
+                            'type': text_space[1],
+                            'align': text_space[3] if len(text_space) > 3 else None,
+                            'Caption': text_space[2]}
+                     }
+    else:
+        parsing_errors = text_space
+        text_dict = None
+    return text_dict, parsing_errors
+
+
+def parse_image_data(ref_text):
+    img_files = parse_data("[[File:", ref_text)
+    return img_files
+
+
+def download_images(image_dict, path):
+    if 'images' not in os.listdir(os.path.join(path)):
+        os.mkdir(os.path.join('images'))
+    img_path = os.path.join(path, 'images')
+    image_links = [(k, v['image_url']) for k, v in image_dict.items()]
+    for (name, url) in image_links:
+        response = requests.get(url)
+        if response.status_code == 200:
+            parser = BeautifulSoup(response.text, 'html.parser')
+            link = parser.find('div', class_="fullImageLink").find('a')
+            url_img = link['href']
+            urllib.request.urlretrieve(url_img, os.path.join(img_path, name))
+            print(f' {name} file written')
+            time.sleep(2)
+        else:
+            print('Image from Url not downloaded:\t', 'Response code:', response.status_code)
+
+
+def extract_images(ref_text):
+    parsing_error = None
+    files = parse_image_data(ref_text)
+    if files:
+        img_files = {}
+        for file in files:
+            img_dict, parsing_error = process_img_tag(file)
+            if img_dict:
+                img_files.update(img_dict)
+    else:
+        img_files = None
+    return img_files, parsing_error
+
+
+def parse_cat_data(ref_text):
+    return parse_data("[[Category:", ref_text)
+
+
+def process_cat_data(cat_text):
+    cat_text = cat_text.replace('| ', '').replace('ory:', '')
+    return cat_text.split('|')
+
+
+def extract_categories(ref_text):
+    files = parse_cat_data(ref_text)
+    return list(itertools.chain.from_iterable([process_cat_data(t) for t in files]))
+
+
+if __name__ == "__main__":
+    from texts import sample_text
+
+    test_link = "https://en.wikipedia.org/wiki/Overview_of_gun_laws_by_nation"
+    test_files = extract_categories(sample_text)
+    test_img_files, parsing_errors = extract_images(sample_text)
+    print('Parsing errors', parsing_errors)
+    print('Test cat files', test_files)
+    # print('Full image dict')
+    # print(test_img_files)
+    print('Test Image files')
+    for k, v in test_img_files.items():
+        print(k, '\n', v)
+    download_images(test_img_files, os.getcwd())
